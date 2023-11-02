512ee092420d1a99f56f885aca57c647ebb0460b
Aishah1q <148686153+Aishah1q@users.noreply.github.com>
2023-11-02 11:40:30 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        //1
+       /* int num1=input.nextInt();
+        int num2=input.nextInt();
+        int num3=input.nextInt();
+        if(num1>num2)
+            System.out.println(num1);
+        else if (num2>num1) {
+            System.out.println(num2);
+        }
+        else {
+            System.out.println(num3);
+        }*/
+
+
+        //2
+     /*   String names=input.next();
+        while (names.length()!=2){
+           
+
+
+        }*/
+
+        //3
+       /*System.out.println("enter a text");
+        String name1=input.next();*/
+
+
+
+
+        }
+        }
+
+
+
+
